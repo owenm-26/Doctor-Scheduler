c9c1ab8d426cb2be576a2ba0bd7fd9918a1b8a55
Owen Mariani <mariani.owen@gmail.com>
2024-10-14 15:08:38 -0400

    changed GET

--- client/src/app/api/launchPythonAdder/route.ts
+++ client/src/app/api/launchPythonAdder/route.ts
@@ -2,28 +2,23 @@ import { exec } from "child_process";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  return new Promise((resolve) => {
-    exec(
-      "python3 /Users/owenmariani/Desktop/Streching-Pal/backend/posegui2.py",
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error executing Python script: ${error.message}`);
-          resolve(
-            NextResponse.json({ error: "Failed to execute" }, { status: 500 })
-          );
-          return;
-        }
-        if (stderr) {
-          console.error(`Script error output: ${stderr}`);
-          resolve(
-            NextResponse.json({ error: "Script error" }, { status: 500 })
-          );
-          return;
-        }
-        // Success, send back the output
-        console.log(`Script output: ${stdout}`);
-        resolve(NextResponse.json({ message: stdout }, { status: 200 }));
+  exec(
+    "python3 /Users/owenmariani/Desktop/Streching-Pal/backend/posegui2.py",
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing Python script: ${error.message}`);
+        return NextResponse.json(
+          { error: "Failed to execute" },
+          { status: 500 }
+        );
       }
-    );
-  });
+      if (stderr) {
+        console.error(`Script error output: ${stderr}`);
+        return NextResponse.json({ error: "Script error" }, { status: 500 });
+      }
+      // Success, send back the output
+      console.log(`Script output: ${stdout}`);
+      return NextResponse.json({ message: stdout }, { status: 200 });
+    }
+  );
 };
